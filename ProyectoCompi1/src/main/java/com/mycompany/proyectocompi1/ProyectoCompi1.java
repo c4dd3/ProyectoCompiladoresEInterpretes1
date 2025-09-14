33e034faf56d4c660426625334037133fcff5fa2
Danielo Wu <danielowu1012@gmail.com>
2025-09-14 09:37:02 -0600

    First Commit

---
+++ ProyectoCompi1/src/main/java/com/mycompany/proyectocompi1/ProyectoCompi1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyectocompi1;
+
+/**
+ *
+ * @author danie
+ */
+public class ProyectoCompi1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
